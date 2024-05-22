4a15ceaf90beb0b38e634add8412b4a918ec009a
SuiiPanda <61253137+SuiiPanda@users.noreply.github.com>
2024-05-22 10:12:04 +0100

    Update SchoolSetup.sql
    Fixed issue with dropping table and checking for existing db before creating

--- SchoolSetup.sql
+++ SchoolSetup.sql
@@ -1,3 +1,11 @@
+USE master
+GO
+
+IF EXISTS (SELECT * FROM sysdatabases WHERE NAME = 'LanguageSchool')
+	ALTER DATABASE LanguageSchool SET SINGLE_USER
+	DROP DATABASE LanguageSchool
+GO
+
 CREATE DATABASE LanguageSchool
 GO
 
@@ -9,7 +17,7 @@ CREATE TABLE Staff(
 	Firstname NVARCHAR(30) NOT NULL,
 	Surname NVARCHAR(30) NOT NULL,
 	[Role] NVARCHAR(30) NOT NULL,
-	Salary DECIMAL(8)
+	Salary DECIMAL(8,2)
 )
 GO
 
@@ -22,37 +30,25 @@ CREATE TABLE Cars(
 GO
 
 INSERT INTO Staff(Firstname, Surname, [Role], Salary)
-VALUES('Martina', 'Jenkins', 'DoS', 23000)
-INSERT INTO Staff(Firstname, Surname, [Role], Salary)
-VALUES('Simon', 'Jenkins', 'Teacher', 21000)
-INSERT INTO Staff(Firstname, Surname, [Role], Salary)
-VALUES('Lorraine', 'Jenkins', 'Teacher', 18000)
-INSERT INTO Staff(Firstname, Surname, [Role], Salary)
-VALUES('Lucy', 'Young', 'Receptionist', 16000)
-INSERT INTO Staff(Firstname, Surname, [Role], Salary)
-VALUES('James', 'Sunderland', 'Teacher', 20000)
-INSERT INTO Staff(Firstname, Surname, [Role], Salary)
-VALUES('Angela', 'Orosco', 'Teacher', 19000)
-INSERT INTO Staff(Firstname, Surname, [Role], Salary)
-VALUES('Eddie', 'Dumbrowski', 'Teacher', 18000)
-INSERT INTO Staff(Firstname, Surname, [Role], Salary)
-VALUES('Walter', 'Sullivan', 'Warden', 15000)
-INSERT INTO Staff(Firstname, Surname, [Role], Salary)
-VALUES('Simon', 'Jenkins', 'Accommodation Officer', 17000)
+VALUES
+	('Martina', 'Jenkins', 'DoS', 23000),
+	('Simon', 'Jenkins', 'Teacher', 21000),
+	('Lorraine', 'Jenkins', 'Teacher', 18000),
+	('Lucy', 'Young', 'Receptionist', 16000),
+	('James', 'Sunderland', 'Teacher', 20000),
+	('Angela', 'Orosco', 'Teacher', 19000),
+	('Eddie', 'Dumbrowski', 'Teacher', 18000),
+	('Walter', 'Sullivan', 'Warden', 15000),
+	('Simon', 'Jenkins', 'Accommodation Officer', 17000)
 GO
 
 INSERT INTO Cars(RegNo, Colour, Make, [Owner])
-VALUES('AA11777', 'Blue', 'Audi', 10005)
-INSERT INTO Cars(RegNo, Colour, Make, Owner)
-VALUES('JJ66CUP', 'Silver', 'Citroen', 10007)
-INSERT INTO Cars(RegNo, Colour, Make, Owner)
-VALUES('PK44TUL', 'Red', 'Mercedes', 10001)
-INSERT INTO Cars(RegNo, Colour, Make, Owner)
-VALUES('QQ34BBB', 'Blue', 'Ford', 10006)
-INSERT INTO Cars(RegNo, Colour, Make, Owner)
-VALUES('SA33USB', 'White', 'Seat', 10008)
-INSERT INTO Cars(RegNo, Colour, Make, Owner)
-VALUES('XY21ONO', 'Blue', 'Ford', 10005)
-INSERT INTO Cars(RegNo, Colour, Make, Owner)
-VALUES('ZZ9PZA', 'Red', 'Audi', 10003)
+VALUES
+	('AA11777', 'Blue', 'Audi', 10005),
+	('JJ66CUP', 'Silver', 'Citroen', 10007),
+	('PK44TUL', 'Red', 'Mercedes', 10001),
+	('QQ34BBB', 'Blue', 'Ford', 10006),
+	('SA33USB', 'White', 'Seat', 10008),
+	('XY21ONO', 'Blue', 'Ford', 10005),
+	('ZZ9PZA', 'Red', 'Audi', 10003)
 GO
